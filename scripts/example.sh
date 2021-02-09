e566fe932958c7ed5cd958937831e4bc327d3c47
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-02-09 15:50:54 +0700

    Initial commit

---
+++ scripts/example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is just a placeholder file, to ensure that the shellcheck GitHub Action does not fail
+# saying there are no files found (for it to check).
+# So this file can be removed as soon as the repo has at least one other shell script.
